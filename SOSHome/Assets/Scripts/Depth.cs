ac792b7e7e0acfcb46d4390b400cf6a66bfc3a5b
Thiago Henrique <thiagohrh@gmail.com>
2019-01-26 18:15:31 -0200

    HABEAMUS BUILD
    FOI

---
+++ SOSHome/Assets/Scripts/Depth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Depth : MonoBehaviour
+{
+
+    public SpriteRenderer sprRenderer;
+
+    public float depthPrecision;
+    public float localLayer;
+
+    public static Depth depthHandler;
+
+    void Start()
+    {
+        depthHandler = this;
+        sprRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sprRenderer.sortingOrder = (int)(-transform.position.y * depthPrecision) + (int)localLayer;
+    }
+}
