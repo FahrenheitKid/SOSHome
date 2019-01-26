e7a6afe9d2030b6bd746c33c370432d4d5390b60
Thiago Henrique <thiagohrh@gmail.com>
2019-01-26 09:53:44 -0200

    Basic setting for the entity AI.
    About to start working on the NPC.

--- SOSHome/Assets/Characters/AiLifeform.cs
+++ SOSHome/Assets/Characters/AiLifeform.cs
@@ -6,8 +6,13 @@ public class AiLifeform : MonoBehaviour
 {
     public string type;
 
-    private float velocity = 6.0f;
+    public float velocity = 6.0f;
+    protected Vector3 startingPosition = new Vector3(0,0,0);
     public float rangeDelim = 40.0f;
+
+    protected enum States { IDLE, MOVING };
+
+    protected States current_state = States.IDLE;
 
     void Initialize() {
         //Better to write your own code to control starting variables.
@@ -16,7 +21,7 @@ public class AiLifeform : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -25,17 +30,34 @@ public class AiLifeform : MonoBehaviour
         
     }
 
-    Vector3 GetRandomMovementPosition() {
+    protected Vector3 GetRandomMovementPosition() {
         //This function is meant to return a Vector3 that is a point inside a range delimited by the rangeDelim variable.
         //That point is then used to instantiate (or move in case it has already been instantiated) a trigger to tell if
         //that point already has a solid structure. If it DOESNT have, set that point as a destination in a NavMesh agent.
         //Never change the Y position!
         Vector3 finalDestination = new Vector3(0, transform.position.y, 0);
-        float n_z = Random.Range(transform.position.z, rangeDelim);
-        float n_x = Random.Range(transform.position.x, rangeDelim);
+
+        //There must be a more organized way to do this but...I am tired.
+        int n = Random.Range(0,1);
+        float n_z = 0.0f;
+        float n_x = 0.0f;
+
+        if (n == 0)
+            n_z = Random.Range(startingPosition.z, rangeDelim);
         
+        else 
+            n_z = Random.Range(startingPosition.z, -rangeDelim);
+        
+        n = Random.Range(0, 1);
+        if (n == 0)
+            n_x = Random.Range(startingPosition.x, rangeDelim);
+        
+        else
+            n_x = Random.Range(startingPosition.x, -rangeDelim);
+
+        finalDestination.z = n_z;
+        finalDestination.x = n_x;
 
         return finalDestination;
     }
-
 }
---
+++ SOSHome/Assets/Characters/NPCs/IA_NPC.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA_NPC : AiLifeform
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (current_state == States.IDLE)
+            IdleBehaviour();
+        
+        if (current_state == States.MOVING)
+            MovingBehaviour();
+    }
+
+    void IdleBehaviour() {
+        //Just stops for a couple of seconds.
+    }
+
+    void MovingBehaviour() {
+        //Uses GetRandomMovementPosition to get a random point around the entity, checks if it is an empty space, moves towards it using NavMesh.
+        GetRandomMovementPosition();
+    }
+}
