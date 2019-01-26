476dbe7a8d375f254c71dde1895d20fd2aec24c8
Thiago Henrique <thiagohrh@gmail.com>
2019-01-26 04:28:44 -0200

    Starting with a function used for the AI of the NPC.
    Will finish up tomorow.

--- SOSHome/Assets/Characters/AiLifeform.cs
+++ SOSHome/Assets/Characters/AiLifeform.cs
@@ -7,11 +7,12 @@ public class AiLifeform : MonoBehaviour
     public string type;
 
     private float velocity = 6.0f;
-
+    public float rangeDelim = 40.0f;
 
     void Initialize() {
-
+        //Better to write your own code to control starting variables.
     }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,4 +24,18 @@ public class AiLifeform : MonoBehaviour
     {
         
     }
+
+    Vector3 GetRandomMovementPosition() {
+        //This function is meant to return a Vector3 that is a point inside a range delimited by the rangeDelim variable.
+        //That point is then used to instantiate (or move in case it has already been instantiated) a trigger to tell if
+        //that point already has a solid structure. If it DOESNT have, set that point as a destination in a NavMesh agent.
+        //Never change the Y position!
+        Vector3 finalDestination = new Vector3(0, transform.position.y, 0);
+        float n_z = Random.Range(transform.position.z, rangeDelim);
+        float n_x = Random.Range(transform.position.x, rangeDelim);
+        
+
+        return finalDestination;
+    }
+
 }
